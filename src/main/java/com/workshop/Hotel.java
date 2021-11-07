c00545da48326f3ea613db1791046f59b44c0c91
aajisahu <aajisahu.com>
2021-11-07 18:11:20 +0530

    Added: Ability to find cheapest hotel for a given data range

--- src/main/java/com/workshop/Hotel.java
+++ src/main/java/com/workshop/Hotel.java
@@ -1,29 +1,8 @@
 package com.workshop;
 
-/**
- * 
- * @author ASHISH SAHU
- *this class is used to set value and set values 
- */
 public class Hotel {
     private String hotelName;
-    private String customerType;
-    private int weekdayRatesForRegularCustomer;
-    private int weekendPriceForRegularCustomer;
-    private int weekdayRatesForRewardCustomer;
-    private int weekendPriceForRewardCustomer;
-    private int rating;
-    //Parameterized constructor to initialize instance variable
-    public Hotel(String hotelName, String customerType, int weekdayRatesForRegularCustomer,
-                 int weekendPriceForRegularCustomer, int weekdayRatesForRewardCustomer, int weekendPriceForRewardCustomer, int rating) {
-        this.hotelName = hotelName;
-        this.customerType = customerType;
-        this.weekdayRatesForRegularCustomer = weekdayRatesForRegularCustomer;
-        this.weekendPriceForRegularCustomer = weekendPriceForRegularCustomer;
-        this.weekdayRatesForRewardCustomer = weekdayRatesForRewardCustomer;
-        this.weekendPriceForRewardCustomer = weekendPriceForRewardCustomer;
-        this.rating = rating;
-    }
+    private int hotelRegularRate;
 
     public String getHotelName() {
         return hotelName;
@@ -33,51 +12,17 @@ public class Hotel {
         this.hotelName = hotelName;
     }
 
-    public String getCustomerType() {
-        return customerType;
+    public int getHotelRegularRate() {
+        return hotelRegularRate;
     }
 
-    public void setCustomerType(String customerType) {
-        this.customerType = customerType;
+    public void setHotelRegularRate(int hotelRegularRate) {
+        this.hotelRegularRate = hotelRegularRate;
     }
 
-    public int getWeekdayRatesForRegularCustomer() {
-        return weekdayRatesForRegularCustomer;
-    }
+    public Hotel(String hotelName, int hotelRegularRate) {
+        this.hotelName = hotelName;
+        this.hotelRegularRate = hotelRegularRate;
 
-    public void setWeekdayRatesForRegularCustomer(int weekdayRatesForRegularCustomer) {
-        this.weekdayRatesForRegularCustomer = weekdayRatesForRegularCustomer;
-    }
-
-    public int getWeekendPriceForRegularCustomer() {
-        return weekendPriceForRegularCustomer;
-    }
-
-    public void setWeekendPriceForRegularCustomer(int weekendPriceForRegularCustomer) {
-        this.weekendPriceForRegularCustomer = weekendPriceForRegularCustomer;
-    }
-
-    public int getWeekdayRatesForRewardCustomer() {
-        return weekdayRatesForRewardCustomer;
-    }
-
-    public void setWeekdayRatesForRewardCustomer(int weekdayRatesForRewardCustomer) {
-        this.weekdayRatesForRewardCustomer = weekdayRatesForRewardCustomer;
-    }
-
-    public int getWeekendPriceForRewardCustomer() {
-        return weekendPriceForRewardCustomer;
-    }
-
-    public void setWeekendPriceForRewardCustomer(int weekendPriceForRewardCustomer) {
-        this.weekendPriceForRewardCustomer = weekendPriceForRewardCustomer;
-    }
-
-    public int getRating() {
-        return rating;
-    }
-
-    public void setRating(int rating) {
-        this.rating = rating;
     }
 }
--- src/main/java/com/workshop/HotelReservationSystem.java
+++ src/main/java/com/workshop/HotelReservationSystem.java
@@ -1,23 +1,35 @@
 package com.workshop;
-
 import java.util.ArrayList;
-/*
-* Program : Hotel reservation System
-* Author : Navalkumar
-* Date : 23/10/2021
-* */
-public class HotelReservationSystem {
-    // ArrayList to store Hotel details
-    public ArrayList<Hotel> hotelList = new ArrayList<Hotel>();
+import java.util.List;
+/**
+ * Purpose  - Ability to find cheapest hotel for a given data range
 
-    public static void main(String[] args) {
-        System.out.println("Welcome to Hotel Reservation System ");
+ */
+
+public class HotelReservationSystem {
+    private List<Hotel> hotels;
+
+    public HotelReservationSystem(){
+        this.hotels = new ArrayList<Hotel>();
     }
 
-    //Created method to add hotes in the list
-    public void addHotelDetails() {
-        hotelList.add(new Hotel("Lakewood", "Regular",110,90,80,80,3));
-        hotelList.add(new Hotel("Bridgewood ", "Regular",160,110,60,50,4));
-        hotelList.add(new Hotel("Lakewood", "Regular",220,100,150,40,5));
+    /**
+     * This method is used to add hotel to the Hotel List
+     */
+    public boolean addHotel(Hotel hotel) {
+        return this.hotels.add(hotel);
+    }
+
+    /**
+     * This method is used to find the cheapest hotel in the HotelList
+     * @return - cheapestHotel - The cheapest hotel in the list
+     */
+    public int findCheapestHotel(){
+        int LakeWoodRate = 110; int BridgeWoodRate = 160; int Ridgewood = 220;
+        int cheapestHotel = (LakeWoodRate>BridgeWoodRate) ?
+                (LakeWoodRate>Ridgewood ? LakeWoodRate : Ridgewood) :
+                (BridgeWoodRate>Ridgewood ? BridgeWoodRate : Ridgewood) ;
+        System.out.println("The cheapest Hotel is " +cheapestHotel);
+        return cheapestHotel;
     }
 }
